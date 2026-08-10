4fa46253d3ce01806b61b3ccdbc445230d31487a
Robiul <robiulislam7854@gmail.com>
2026-08-10 14:45:14 +0600

    added reuseable response functionality & ResponseMessaged trait

--- src/main.rs
+++ src/main.rs
@@ -4,89 +4,10 @@ use std::str::Bytes;
 use chrono::{DateTime, Local};
 
 mod request;
-
-#[derive(Debug)]
-enum StatusMessage{
-    Ok,//200
-    BadRequest,//400
-}
-
-// #[derive(Debug)]
-// enum ContentType{
-//     ApplicationJson,
-//     TextHtml,
-//     TextPlain,
-// }
-
-#[derive(Debug)]
-struct ResponseEntityBody<'a>{
-    body:&'a str,
-}
-
-#[derive(Debug)]
-struct StatusLine{
-    version:String,
-    status_code:u32,
-    message:StatusMessage,
-}
-
-#[derive(Debug)]
-struct ResponseHeaderLines{
-    connection:String,
-    date:String,
-    content_length:usize,
-    content_type:String,
-}
-
-#[derive(Debug)]
-struct Response<'a>{
-    status_line:StatusLine,
-    header_lines:ResponseHeaderLines,
-    blank_line:&'a str,
-    body:ResponseEntityBody<'a>,
-}
-
-
+mod respone;
 
 fn main()->Result<()>{
     let litstener = TcpListener::bind("127.0.0.1:8080")?;
-
-    
-//    let todays_date = Local::now().format("%Y-%m-%d %H:%M:%S").to_string();
-    
-    let status_line = StatusLine{
-        version:String::from("HTTP/1.1"),
-        status_code:200,
-        message:StatusMessage::Ok,
-    };
-
-    let content = "my name is robiul";
-    let mut content_len = 0;
-
-    for ch in content.chars(){
-        content_len+=1;
-    }
-
-    let header_lines = ResponseHeaderLines{
-        connection:String::from("Close"),
-        date:Local::now().format("%Y-%m-%d %H:%M:%S").to_string(),
-        content_length:content_len,
-        content_type:String::from("application/json"),
-    };
-
-    let body = ResponseEntityBody{
-        body:content,
-    };
-
-    let response = Response{
-        status_line,
-        header_lines,
-        blank_line:"",
-        body,
-    };
-
-    let response = format!("{} {} {:?}\r\nconnection: {}\r\ndate: {}\r\ncontent-length: {}\r\ncontent-type: {}\r\n\r\n{}",response.status_line.version,response.status_line.status_code,response.status_line.message,response.header_lines.connection,response.header_lines.date,response.header_lines.content_length,response.header_lines.content_type,response.body.body);
-
     
     for stream in litstener.incoming(){
         let mut buffer = [0;4096];
@@ -96,8 +17,12 @@ fn main()->Result<()>{
 
         let info =  request::request::Request::request(buffer, bytes);
 
+        let response = respone::response::Response::response();
+
+
         stream.write_all(response.as_bytes())?;
 
+        
        
     }
     
---
+++ src/respone/response.rs
@@ -0,0 +1,112 @@
+pub mod Response{
+    use crate::Local;
+
+    pub trait ResponseMessage {
+        fn message(&self)->String;
+    }
+
+    #[derive(Debug)]
+    enum StatusMessage{
+        Ok,//200
+        BadRequest,//400
+    }
+
+    #[derive(Debug)]
+    struct ResponseEntityBody<'a>{
+        body:&'a str,
+    }
+
+    impl<'a> ResponseEntityBody<'a> {
+        fn new(body:&'a str)->Self{
+            ResponseEntityBody { body }
+        }
+    }
+
+
+    #[derive(Debug)]
+    struct StatusLine{
+        version:String,
+        status_code:u32,
+        message:StatusMessage,
+    }
+
+    impl StatusLine {
+        fn new(version:String,status_code:u32,message:StatusMessage)->Self{
+            StatusLine { version, status_code, message }
+        }
+    }
+
+
+    #[derive(Debug)]
+    struct ResponseHeaderLines{
+        connection:String,
+        date:String,
+        content_length:usize,
+        content_type:String,
+    }
+
+    impl ResponseHeaderLines {
+        fn new(connection:String,date:String,content_length:usize,content_type:String)->Self{
+            ResponseHeaderLines { connection, date, content_length, content_type }
+        }
+    }
+
+
+    #[derive(Debug)]
+    struct Response<'a>{
+        status_line:StatusLine,
+        header_lines:ResponseHeaderLines,
+        blank_line:&'a str,
+        body:ResponseEntityBody<'a>,
+    }
+
+
+    impl<'a> Response<'a> {
+        fn new(status_line:StatusLine,header_lines:ResponseHeaderLines,blank_line:&'a str,body:ResponseEntityBody<'a>)->Self{
+            Response { status_line, header_lines, blank_line, body }
+        }
+    }
+
+    impl<'a> ResponseMessage for Response<'a> {
+        fn message(&self)->String {
+            return format!("{} {} {:?}\r\nconnection: {}\r\ndate: {}\r\ncontent-length: {}\r\ncontent-type: {}\r\n\r\n{}",self.status_line.version,self.status_line.status_code,self.status_line.message,self.header_lines.connection,self.header_lines.date,self.header_lines.content_length,self.header_lines.content_type,self.body.body);
+        }
+    }
+
+    pub fn response()->String{
+        let status_line = StatusLine::new(
+            String::from("HTTP/1.1"), 
+            200, 
+            StatusMessage::Ok,
+        );
+
+        let content = "name: my name is robiul";
+        let mut content_len = 0;
+
+        for ch in content.chars(){
+            content_len+=1;
+        }
+
+        let header_lines = ResponseHeaderLines::new(
+            String::from("Close"),
+            Local::now().format("%Y-%m-%d %H:%M:%S").to_string(),
+            content_len,
+            String::from("application/json"),
+        );
+
+        let body = ResponseEntityBody::new(content);
+
+        let response = Response::new(
+            status_line,
+            header_lines,
+            "",
+            body
+        );
+
+        response.message()
+
+
+    }
+    
+
+}
